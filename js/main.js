842a7c3ba11229506b1fe0843caf970ac76fd82e
Djordje Arsenovic <dj.arsenovic@gmail.com>
2018-08-07 01:02:14 +0200

    Changing sw path

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
   // console.log('Service worker supported');
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../sw_cached_pages.js')
+      .register('https://djordje47.github.io/simpleServiceWorker/sw_cached_pages.js')
       .then(reg => console.log('SW -> Registered'))
       .catch(err => console.log(err));
   })
